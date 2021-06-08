b148dacbdafec29397ae2abe4d84f7b01c05500a
Meaghan Kramer <>
2021-06-08 09:57:49 +0000

    locaties apart bestand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from kamer import kamers
 
 def Verlorenagain():
   print("Je hebt verloren")
@@ -18,67 +19,6 @@ def Winagain():
       print("GAME OVER")
       exit()
 
-
-kamers = {
-  "padkeuze": {
-    "title": "dit spel te laten beginnen",
-    "beschrijving": "Je staat in de modder, met een grote heuvel voor je en een enorme schaduw hangt over je heen. Je denkt al jaren aan die ene legende die je hoorde van je opa. Eindelijk heb je je ouders kunnen overtuigen om je de reis naar de schat te laten gaan maken. De schat ligt op de berg. Je staat hier voor je eerste kruising. Je kan kiezen uit twee paden. Een lang en veilig pad of een kort en onveilig pad. Welk pad kies je?",
-    "options": ["lang en veilig" , "kort en onveilig"],
-    "lost": "nee",
-    "win": "nee"
-    },
-
-  "kort en onveilig": { 
-    "title": "een kort en onveilig pad",
-    "beschrijving": "Je hebt een gewaagde keus gemaakt en kies voor het kort en onveilige pad. Echter is het pad zo onveilig dat je niet meer vooruit kan en niet meer achteruit. Het spel is over",
-    "options": [""],
-    "lost": "ja",
-    "win": "nee",
-    },
-  "lang en veilig": {
-    "title": "het lange en veilig pad",
-    "beschrijving": "Je hebt het zekere voor het onzekere gekozen en gaat verder met je reis.  Op een moment voel je ineens de grond zacht worden en je begint weg te zakken!!! Oh nee!!! Je denkt aan het verhaal dat je opa je had verteld, het is drijfzand! Je kijkt op je heen en ziet de dingen waar je je mee kan redden. Een touw, een tak, heel lang nadenken", 
-    "options": ["een touw", "een tak", "lang nadenken"],
-    "lost": "no",
-    "win": "no",
-    },
-  "lang nadenken": {
-    "title": "heel lang nadenken",
-    "beschrijving": "Na heel lang nadenken weet je het echt niet meer. Je zakt steeds verder in de modder. Nu kan je niet meer terug. De enige weg is terug naar huis. Je reis is helaas voorbij.",
-    "options": [""],
-    "lost": "ja",
-    "win": "nee"
-  },
-
-  "een touw": {
-    "title": "het touw om je te redden",
-    "beschrijving": "Met behulp van het touw lukt het je om uit het drijfzand te komen. Je gaat verder met je reis. Na een paar uur lopen stoot je je voet ineens tegen iets hards! Je gaat kijken wat het is en ziet ineens een hamer uit de grond steken, dit moest vast van de ontdekkingsreizigers zijn die op zoek gingen naar de schat maar nooit meer waren teruggekomen! Zal je de hamer oppakken?.",
-    "options": ["ja", "nee"],
-    "lost": "no",
-    "win": "no",
-  },
-  "een tak": {
-    "title": "het tak om je te redden",
-    "beschrijving": "Met behulp van het touw lukt het je om uit het drijfzand te komen. Je gaat verder met je reis. Na een paar uur lopen stoot je je voet ineens tegen iets hards! Je gaat kijken wat het is en ziet ineens een hamer uit de grond steken, dit moest vast van de ontdekkingsreizigers zijn die op zoek gingen naar de schat maar nooit meer waren teruggekomen! Zal je de hamer oppakken?.",
-    "options": ["ja", "nee"],
-    "lost": "no",
-    "win": "no",
-    },
-  "nee": {
-    "title": "de hamer niet op te pakken",
-    "beschrijving": "je bent net de pijn uit je voet kwijt als je ineens een raar figuur in de verte ziet staan, het lijkt op een bosbewoner! Hij komt op je af met een aardig snel tempo, oh nee wat nu?!!. Je kan de bosbewoner uitschelden, begroeten, of negeren",
-    "options": ["begroeten", "negeren", "uitschelden"],
-    "lost": "no",
-    "win": "no",
-    },
-  "begroeten": {
-    "title": "om rustig te blijven en het wezen te begroeten",
-    "beschrijving": "je besluit toch maar even de kaart van je opa te bekijken, je bent al over de helft van de route naar de schat! Je ziet ineens de bloedvlekken op de kaart die je eerder nog niet zijn ogevallen, je hoort gegrom boven je en kijkte omhoog, het is een woeste Dodo! Maar die waren toch uitgestorven, hier niet! Wat doe je? dood maken of rustig blijven",
-    "options": ["dood maken", "rustig blijven"],
-    "lost": "no",
-    "win": "no"
-    }
-} 
 def Soortenkamers(kamer):
   huidigekamer = kamers[kamer] 
   titel = huidigekamer["title"]
@@ -102,8 +42,6 @@ def Soortenkamers(kamer):
   print("Waarvoor kies je: ")
   print(" , ".join(options))
   print('-' * 80)
-
-    
   
   Volgende = input()
   if Volgende == "q" or Volgende == "Q":
