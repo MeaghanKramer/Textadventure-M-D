3d0b26a76056fce4fa01fc2946f03a18db8fdd5f
Meaghan Kramer <74553139+MeaghanKramer@users.noreply.github.com>
2021-06-10 19:33:40 +0000

    Commants van 1 letter (dus ook kamer namen 1 letter)

--- kamer.py
+++ kamer.py
@@ -1,83 +1,83 @@
 kamers = {
   "padkeuze": {
     "title": "dit spel te laten beginnen",
-    "beschrijving": "Je staat in de vieze modder, met een grote heuvel voor je en een enorme schaduw hangt over je heen. #scarystuff Je denkt al jaren aan die ene legende die je hoorde van je opa. Eindelijk heb je je ouders kunnen overtuigen om je de reis naar de schat te laten maken. #manipulatie. De schat ligt op de berg... Je staat hier voor je eerste kruising. Je kan kiezen uit twee paden. Een lang en veilig pad of een kort en onveilig pad. Welk pad kies je? #SPANNUHND",
-    "options": ["lang en veilig" , "kort en onveilig"],
-    "items" : "",
+    "beschrijving": "Je staat in de vieze modder, met een grote heuvel voor je en een enorme schaduw hangt over je heen. #scarystuff Je denkt al jaren aan die ene legende die je hoorde van je opa. Eindelijk heb je je ouders kunnen overtuigen om je de reis naar de schat te laten maken. #manipulatie. De schat ligt op de berg... Je staat hier voor je eerste kruising. Je kan kiezen uit twee paden. Een lang en veilig pad (L) of een kort en onveilig pad (K). Welk pad kies je? #SPANNUHND",
+    "options": ["l" , "k"],
+   # "items" : "",
     "lost": "nee",
     "win": "nee"
     },
 
-  "kort en onveilig": { 
+  "k": { 
     "title": "een kort en onveilig pad",
     "beschrijving": "Je hebt een gewaagde keus gemaakt en kies voor het kort en onveilige pad.#rebel. Dit pad is zo extreem gevaarlijk... je valt gelijk flauw! Nee grapje, je gaat na tien meter lopen dood door een gas in de lucht, je realiseert je dit pas te laat... #letopjeomgeving. Het spel is over",
     "options": [""],
-    "items" : "",
+    #"items" : "",
     "lost": "ja",
     "win": "nee",
     },
-  "lang en veilig": {
+  "l": {
     "title": "het lange en veilig pad",
-    "beschrijving": "Je hebt het zekere voor het onzekere gekozen en gaat verder met je reis, dan maar iets langer lopen. Op een moment voel je ineens de grond zacht worden en je begint weg te zakken!!! Oh nee!!! Je denkt aan het verhaal dat je opa je had verteld, het is toch echt drijfzand en niet de scetchers met dat zachte voetkussen! Je kijkt op je heen en ziet de dingen waar je je mee kan redden. Een touw, een tak, heel lang nadenken", 
-    "options": ["touw", "tak", "lang nadenken"],
-    "items" : "",
+    "beschrijving": "Je hebt het zekere voor het onzekere gekozen en gaat verder met je reis, dan maar iets langer lopen. Op een moment voel je ineens de grond zacht worden en je begint weg te zakken!!! Oh nee!!! Je denkt aan het verhaal dat je opa je had verteld, het is toch echt drijfzand en niet de scetchers met dat zachte voetkussen! Je kijkt op je heen en ziet de dingen waar je je mee kan redden. Een touw (R), een tak (T), heel lang nadenken (D)", 
+    "options": ["r", "t", "d"],
+   # "items" : "",
     "lost": "no",
     "win": "no",
     },
-  "lang nadenken": {
+  "d": {
     "title": "heel lang nadenken",
     "beschrijving": "Na heel lang nadenken weet je het echt niet meer. Je zakt steeds verder in de modder.#uitstelgedrag Nu kan je niet meer terug...  Al snel krijg je geen adem meer en sterf je... (hoe dom ben je ook dat je stopt met ademen #loser)",
     "options": [""],
-    "items" : "",
+   # "items" : "",
     "lost": "ja",
     "win": "no"
   },
 
-  "touw": {
+  "r": {
     "title": "het touw om je te redden",
-    "beschrijving": "Met behulp van het touw lukt het je om uit het drijfzand te komen. Je gaat verder met je reis. Na een paar uur lopen stoot je je voet ineens tegen iets hards! #letopwaarjeloopt #nietzonaarjetelefoonstaren. Je gaat kijken wat het is en ziet ineens een hamer uit de grond steken (IS THOR HIER GEWEEST?!!), dit moest vast van de ontdekkingsreizigers zijn die op zoek gingen naar de schat maar nooit meer waren teruggekomen! Zal je de hamer oppakken? (of een betere vraag, KAN je de hamer oppakken? #onlytheworthy",
-    "options": ["hamer wel oppakken", "hamer niet oppakken"],
-    "items" : "",
+    "beschrijving": "Met behulp van het touw lukt het je om uit het drijfzand te komen. Je gaat verder met je reis. Na een paar uur lopen stoot je je voet ineens tegen iets hards! #letopwaarjeloopt #nietzonaarjetelefoonstaren. Je gaat kijken wat het is en ziet ineens een hamer uit de grond steken (IS THOR HIER GEWEEST?!!), dit moest vast van de ontdekkingsreizigers zijn die op zoek gingen naar de schat maar nooit meer waren teruggekomen! Zal je de hamer oppakken? (of een betere vraag, KAN je de hamer oppakken?Keuze: oppakken (O), en niet oppakken (N) #onlytheworthy",
+    "options": ["o", "n"],
+    #"items" : "",
     "lost": "no",
     "win": "no",
   },
-  "tak": {
+  "t": {
     "title": "tak" ,
-    "beschrijving":"Met behulp van het touw lukt het je om uit het drijfzand te komen. Je gaat verder met je reis, terwijl je armen pijn doen... Na een paar uur lopen stoot je je voet ineens tegen iets hards! #letopwaarjeloopt #doesniet Je gaat kijken wat het is en ziet ineens een hamer uit de grond steken (wie is er ook zo dom om zn hamer hier achter te laten, pffff), dit moest vast van de ontdekkingsreizigers zijn die op zoek gingen naar de schat maar nooit meer waren teruggekomen! #ohoh Zal je de hamer oppakken? (of een betere vraag, KAN je de hamer oppakken? #onlytheworthy",
-    "options": ["hamer wel oppakken", "hamer niet oppakken"],
-    "items" : ["hamer"],
+    "beschrijving":"Met behulp van het touw lukt het je om uit het drijfzand te komen. Je gaat verder met je reis, terwijl je armen pijn doen... Na een paar uur lopen stoot je je voet ineens tegen iets hards! #letopwaarjeloopt #doesniet Je gaat kijken wat het is en ziet ineens een hamer uit de grond steken (wie is er ook zo dom om zn hamer hier achter te laten, pffff), dit moest vast van de ontdekkingsreizigers zijn die op zoek gingen naar de schat maar nooit meer waren teruggekomen! #ohoh Zal je de hamer oppakken? (of een betere vraag, KAN je de hamer oppakken? Keuze: oppakken (O), en niet oppakken (N) #onlytheworthy",
+    "options": ["o", "n"],
+    #"items" : ["hamer"],
     "lost": "nee",
     "win": "nee",
     },
-  "hamer niet oppakken": {
+  "n": {
     "title": "de hamer niet op te pakken",
-    "beschrijving": "Je bent net de pijn uit je voet kwijt als je ineens een raar figuur in de verte ziet staan, het lijkt op een bosbewoner!(Je ruikt hem helemaal hier!!! Wanneer heeft hij voor het laatst gedouchet? Of wacht, ben jij dat zelf? #stinkiewinkie) Hij komt op je af met een aardig snel tempo, oh nee wat nu?!!. Je kan de bosbewoner uitschelden, begroeten, of negeren, je kan ook terug naar de vorige keuze",
-    "options": ["begroeten", "negeren", "uitschelden"],
+    "beschrijving": "Je bent net de pijn uit je voet kwijt als je ineens een raar figuur in de verte ziet staan, het lijkt op een bosbewoner!(Je ruikt hem helemaal hier!!! Wanneer heeft hij voor het laatst gedouchet? Of wacht, ben jij dat zelf? #stinkiewinkie) Hij komt op je af met een aardig snel tempo, oh nee wat nu?!!. Je kan de bosbewoner uitschelden (U), begroeten (B), of negeren (S), je kan ook terug naar de vorige keuze",
+    "options": ["b", "s", "u"],
     "lost": "nee",
     "win": "nee",
     },
-  "hamer wel oppakken"  : { 
+  "o"  : { 
     "title": "de hamer wel op te pakken",
-    "beschrijving": "Je besluit toch maar even de kaart van je opa te bekijken, je bent al over de helft van de route naar de schat! Je ziet ineens de bloedvlekken op de kaart die je eerder nog niet zijn ogevallen (of ketchup?), je hoort gegrom boven je en kijkte omhoog (Nee, DAT IS BLOED!), het is een woeste Dodo! Maar die waren toch al 39837383 eeuwen geldeden uitgestorven, hier niet! #mutatie #scarydodo #dodododen Wat doe je?",
-    "options" : ["rustig blijven", "dodo doden"],
+    "beschrijving": "Je besluit toch maar even de kaart van je opa te bekijken, je bent al over de helft van de route naar de schat! Je ziet ineens de bloedvlekken op de kaart die je eerder nog niet zijn ogevallen (of ketchup?), je hoort gegrom boven je en kijkte omhoog (Nee, DAT IS BLOED!), het is een woeste Dodo! Maar die waren toch al 39837383 eeuwen geldeden uitgestorven, hier niet! #mutatie #scarydodo #dodododen Wat doe je? Rustig blijven (C) of dodo doden (F)",
+    "options" : ["c", "f"],
     "lost" : "nee",
     "win" : "nee"
   },
-  "rustig blijven" : {
+  "c" : {
     "title" : "rustig te blijven",
-    "beschrijving": "Doordat je zo rustig blijft, lukt het je om de dodo te temmen. Je hebt een nieuw maatje voor het leven en die met je verder reist op zoek naar de schat. #pakezel. De Dodo begint ineens een hard geluid te maken en je vraagt je af wat het is, je kijkt naar voren en ziet een enorme boomstam liggen. AIIII NOOO! Hoe moet je hier overheen komen?!! #doomed. De Dodo maakt een grote sprong en gaat er in 1x overheen! Wat ga jij doen om verder te komen? (naast huilen)",
-    "options" : ["klimmen", "hakken met hamer"],
+    "beschrijving": "Doordat je zo rustig blijft, lukt het je om de dodo te temmen. Je hebt een nieuw maatje voor het leven en die met je verder reist op zoek naar de schat. #pakezel. De Dodo begint ineens een hard geluid te maken en je vraagt je af wat het is, je kijkt naar voren en ziet een enorme boomstam liggen. AIIII NOOO! Hoe moet je hier overheen komen?!! #doomed. De Dodo maakt een grote sprong en gaat er in 1x overheen! Wat ga jij doen om verder te komen? (naast huilen) Klimmen (A) of hakken met hamer (H)",
+    "options" : ["a", "h"],
     "lost" : "nee",
     "win": "nee",
   },
-   "dodo doden" : {
+   "f" : {
     "title" : "dodo te doden",
-    "beschrijving": "Je doodt het beest met je gevonden hamer en besluit het dier op te eten #JUMMIE, want je had ineens zo een trek! Je negeert het feit dat je een vogel allergie hebt en gaat lekker eten, ach dat moet toch goedkomen!#benjedom Na een lekkere maaltijd moet je kiezen wat je met het kampvuur doet, je keuzes zijn: het kampvuur opruimen, eerst aan laten maar later snel blussen of je ruimt het hele kampvuur niet op.",
-    "options" : ["kampvuur opruimen",  "blussen", "niet opruimen"],
+    "beschrijving": "Je doodt het beest met je gevonden hamer en besluit het dier op te eten #JUMMIE, want je had ineens zo een trek! Je negeert het feit dat je een vogel allergie hebt en gaat lekker eten, ach dat moet toch goedkomen!#benjedom Na een lekkere maaltijd moet je kiezen wat je met het kampvuur doet, je keuzes zijn: het kampvuur opruimen (P), eerst aan laten maar later snel blussen (G) of je ruimt het hele kampvuur niet op (E).",
+    "options" : ["p",  "g", "e"],
     "lost" : "nee",
     "win": "nee",    
  },
-    "kampvuur opruimen" : {
+    "p" : {
     "title" : "kampvuur opruimen",
     "beschrijving": "Je ruimt het kampvuur (zoals het hoort) #preventclimatechange op maar vergeet dat er gevaarlijke leeuwen in de omgeving lopen. Doordat je zolang bij het kampvuur bent gebleven hebben ze de rook gezien en dus nu oo jou!Ze zien je nu als prooi... RENNEN!!!!! Helaas is die leeuw toch echt sneller dan dat jij bent, en die dodo op je maag maakt je nou ook niet bepaald sneller... De leeuw haalt je in en eet je op #sadlife",
     "options" : [],
@@ -85,7 +85,7 @@ kamers = {
     "win": "nee",
     },
 
-    "blussen" : {
+    "g" : {
     "title" : "te blussen",
     "beschrijving": "Na eerst het vuur aan te hebben gedaan, bedenk je je toch en ruim je het kampvuur op. Toch krijg je steeds meer last van je vogel allergie en gaat je lichaam ernstig trillen. Je trilt meer en meer tot dat je ineens de YMCA dance aan het doen bent, wat is dit voor allergie? Of is het een hallicunatie? Het laatste wat je ziet is een dode dodo die je uitlacht terwijl je sterft.#karma ",
     "options" : [],
@@ -93,7 +93,7 @@ kamers = {
     "win": "nee",
     },
 
-    "niet opruimen" : {
+    "e" : {
     "title" : "niet opruimen",
     "beschrijving": "Je kiest ervoor om het kampvuur aan te laten staan want dat heeft toch niet zo gevaar denk je. Alleen dan zie je de dat bomen ook in vuur en vlam staan door het kampvuur en je realiseert dat je een hele bosbrand hebt gesticht... Helaas ben je niet vuur bestendig en verbrand je levend, AaaaRRRHHH!!!!! #nietmetvuurspelen",
     "options" : [],
@@ -102,50 +102,50 @@ kamers = {
     },
 
 
-  "begroeten": {
+  "b": {
     "title": "om rustig te blijven en het wezen te begroeten",
-    "beschrijving": "Je kiest ervoor om aardig te zijn en begroet en het wezen op een aardige toon. #GOEEIEMORGEEN Het wezen begroet je netjes terug. Na een tijdje besluit je toch maar even de kaart van je opa te bekijken, je bent al over de helft van de route naar de schat! Je ziet ineens de bloedvlekken op de kaart die je eerder nog niet zijn ogevallen, je hoort gegrom boven je en kijkte omhoog, het is een woeste Dodo! #DASPECHRUSTWEG #DASBALENNIEUWEKAARTHALEN. Maar dodo's waren toch uitgestorven, hier niet! #GEPRANKT Wat doe je? dodo doden of rustig blijven",
-    "options": ["dodo doden", "rustig blijven"],
+    "beschrijving": "Je kiest ervoor om aardig te zijn en begroet en het wezen op een aardige toon. #GOEEIEMORGEEN Het wezen begroet je netjes terug. Na een tijdje besluit je toch maar even de kaart van je opa te bekijken, je bent al over de helft van de route naar de schat! Je ziet ineens de bloedvlekken op de kaart die je eerder nog niet zijn ogevallen, je hoort gegrom boven je en kijkte omhoog, het is een woeste Dodo! #DASPECHRUSTWEG #DASBALENNIEUWEKAARTHALEN. Maar dodo's waren toch uitgestorven, hier niet! #GEPRANKT Wat doe je? dodo doden (F) of rustig blijven (C)",
+    "options": ["f", "c"],
     "lost": "no",
     "win": "no",
     },
-  "uitschelden": {
+  "u": {
     "title": "uitschelden",
     "beschrijving": "Nadat je het wezen uit hebt gescholden blijkt het wezen ook jouw terug uit te schelden (dodo taal: KJEf jfds  AAHRKFVMO...)Je weet niet wat hij zegt maar het klinkt niet al te aardig #doeslief. De situatie dreigt zeer uit de hand te lopen. Het lijkt erop dat het wezen iets uit zijn jaszak wilt halen. Het ziet er niet goed uit en dat is de reden waarom je snel terug wilt naar de vorige keuze!!! Had je hem nu maar niet uigescholden...",
-    "options" : ["hamer niet oppakken"], 
+    "options" : ["n"], 
     "lost": "nee",
     "win": "nee",
     },
 
-    "negeren": {
+    "s": {
     "title": "negeren",
     "beschrijving": "Nadat je langs het rare figuur bent gelopen, voel je je toch niet zo op je gemak en besluit je terug te gaan naar de vorige keuze om het toch maar anders te benaderen...",
-    "options" : ["hamer niet oppakken"], 
+    "options" : ["n"], 
     "lost": "nee",
     "win": "nee",
     },
-  "klimmen": {
+  "a": {
     "title": "om over de muur te klimmen",
     "beschrijving": "Je probeert over de muur te klimmen maar het lukt niet.#weak Na talloze pogingen raak je moe en besluit je te overnachten bij de muur en morgen nog eens te proberen. Je wordt echter in de nacht opgegeten door de mieren omdat je op hun hutje bent gaan liggen... (Nee maar ff, waarom ga je op een mieren hut liggen #weirdo). Hier eindigt daar je reis op zoek naar de schat, want je bent, nahja, dood.",
     "options": [],
     "lost": "ja",
     "win": "nee",
     },
-  "hakken met hamer": {
+  "h": {
     "title": "om de hamer te gebruiken om de muur over te klimmen",
-    "beschrijving": "Met de hamer die je hebt gevonden maak je de muur... kapot. #slopen Je gaat verder met je reis.Je bent op de top van de berg aangekomen en je vraagt je af waar de schat nou toch zou kunnen liggen! Je kijkt nogmaals naar de kaart en ziet een kruis op de plek waar een grot staat (of is het een bloedvlek?). De grot ziet er groot en donker uit en er is geen licht #OJOOOO. Er komen ook vleermuizen uit zetten die je Dodo met gemak opeet. Wat doe je nu? De grot in of niet", 
-    "options": ["grot in", "niet grot in"],
+    "beschrijving": "Met de hamer die je hebt gevonden maak je de muur... kapot. #slopen Je gaat verder met je reis.Je bent op de top van de berg aangekomen en je vraagt je af waar de schat nou toch zou kunnen liggen! Je kijkt nogmaals naar de kaart en ziet een kruis op de plek waar een grot staat (of is het een bloedvlek?). De grot ziet er groot en donker uit en er is geen licht #OJOOOO. Er komen ook vleermuizen uit zetten die je Dodo met gemak opeet. Wat doe je nu? De grot in (I) of niet(J)", 
+    "options": ["i", "j"],
     "lost": "nee",
     "win": "nee",
     },
-  "grot in": {
+  "i": {
     "title": "de grot in te gaan",
     "beschrijving": "Je kiest ervoor de grot in te gaan. Je ziet de schat voor je liggen en weet dat je het onmogeljke hebt behaald. Je hebt de schat gevonden! #beterdeeljemetme. Och wat zou je opa toch trots zijn!!! Door deze reis ben je ook helemaal fit geworden, maar echt... CHECK DIE BODY!!!! Je leeft verder een prachtig leven met de mooie schat die je hebt gevonden!",
     "options": [],
     "lost": "nee",
     "win": "ja",
     },
-  "grot niet in": {
+  "j": {
     "title": "de grot niet in te gaan",
     "beschrijving": "Je doet een stap naar achter om de grot nog eens goed te bekijken. Je voet slipt weg op de natte stenen en je valt een langzame dood tegemoet aan het einde van de berg. Je was zo dichtbij maar toch zo ver weg... #loser GAME OVER",
     "options": [],
